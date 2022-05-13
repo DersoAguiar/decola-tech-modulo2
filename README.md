ddddf8e22d700f0b7e45bd1e1584b637605ce42a
DersoAguiar <andersonaguiar.m.z@gmail.com>
2022-05-13 19:44:45 -0300

    primeiros passos para desenvolvimento web

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# decola-tech-modulo2
-Atividades práticas feitas no Módulo 2 do Decola Tech da DIO
+# Decola Tech 3ª Edição Módulo 2
+## Primeiros passos para desenvolvimento web
+
+### Ministrado pro Bruno Prado - https://www.linkedin.com/in/bprado/
+
+- Notepad ++
+- w3schools.com
+- code.jquery.com
+- jquery user interface
+- UI - User Interface
+- UX - User Experience
+- HTML - linguagem de marcação
+- WYSIWYG - what you see is what you get
---
+++ xampp/pagina.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>Desafio Accordion</title>
+		
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="/resources/demos/style.css">
+		<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+		<script>
+			$( function() {
+			$( "#accordion" ).accordion();
+			} );
+		</script>
+	
+	</head>
+	<body>
+		
+	<div id="accordion">
+		<h3>Primeira seção</h3>
+		<div><p>texto qualquer 1</p></div>
+		<h3>Segunda Seção</h3>
+		<div><p>texto qualquer 2</p></div>
+		<h3>Terceira Seção</h3>
+		<div><p>texto qualquer 3</p></div>
+		<h3>Quarta Seção</h3>
+		<div><p>texto qualquer 4</p></div>
+	</div>
+	
+	</body>
+</html>
